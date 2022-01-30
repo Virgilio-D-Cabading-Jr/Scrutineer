dae2239f3320790ee0d42c7006c23c4eaf619374
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-30 01:49:24 -0800

    add Subject model in server

--- server/models/subject.model.js
+++ server/models/subject.model.js
@@ -10,4 +10,10 @@ const Matter = require("./matter.model")
 const SubjectSchema = new mongoose.Schema({
     name: String,
     matters: [ Matter ]
-}, {});
+}, {timestamps: true}); // Timestamps implement createdAd/updatedAt
+
+// //// MODEL ///////////////////////////////////
+const Subject = mongoose.model("Subject", SubjectSchema );
+
+// **** Export Model ********
+module.exports = Subject;
