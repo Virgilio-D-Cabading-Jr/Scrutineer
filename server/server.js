9ecdcf859b86ae44bacd13784fb7ced642613e94
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-30 00:25:54 -0800

    initialize connection to db in server.js

--- server/server.js
+++ server/server.js
@@ -12,4 +12,8 @@ const PORT = 8000;
 app.use(express.json(), express.urlencoded({ extended: true }));
 app.use(cors());    // enable Cross Origin Requests in Project
 
+// INITIALIZE SERVER CONNECTION TO DATABASE /////
+require("./config/mongoose.config");
+
+// //// EXECUTE SERVER //////////////////////////
 app.listen(PORT, () => console.log(`🦄🦄🦄 The server is all fired up on port ${PORT} 🦄🦄🦄`));
