ec549891bbce2aac8ec7b660cf188efec4f0c89e
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-02-02 11:37:39 -0800

    add input form to stackMemoryToolView in client

--- client/src/components/ShowStackComp.jsx
+++ client/src/components/ShowStackComp.jsx
@@ -19,13 +19,24 @@ const ShowStackComp = (props) => {
     }
 
     // **** Output *********************************
-    return (<div className='row m-3 border border-primary round'>
+    return (<div className='row p-3 m-3 bg-primary round text-white'>
         <div className='col'>
-            <p>Answers: {JSON.stringify(answers)}</p>
-            <p>Stack: {JSON.stringify(stack)}</p>
             {
-                stack.map(() => {
-                    return <div className='row mb-1'><div className='col-2'>1</div><div className='col-9 bg-warning p-2 round'>question</div></div>
+                stack.map((row, idx) => {
+                    const answerIdx = row[0];
+                    const questionIdx = row[1];
+                    const question = answers[answerIdx].questions[questionIdx]
+                    return <div key={ idx } className='row mb-1'>
+                            <div className='col-2'>{ stack.length-idx }</div>
+                            {
+                                idx == 0
+                                ?   <div className='col-9 bg-warning p-2 round text-center'><h3>{ question }</h3></div>
+                                :   idx % 2 == 0
+                                    ?   <div className='col-9 bg-white text-dark p-2 round text-center'><h3>{ question }</h3></div>
+                                    :   <div className='col-9 bg-info text-dark p-2 round text-center'><h3>{ question }</h3></div>
+                            }
+                            
+                        </div>
                 })
             }
         </div>
--- client/src/views/StackMemoryToolView.jsx
+++ client/src/views/StackMemoryToolView.jsx
@@ -13,20 +13,25 @@ const StackMemoryToolView = (props) => {
     const [toolStarted, setToolStarted] = useState(false);
     const [stack, setStack] = useState([]);
 
+    // **** Utilities ******************************
+
+    // **** Create a New Stack ********
     const initializeRandomStack = () => {
         let copyStack = [];
-        for (let i=0; i<5; i++) {
+        for (let i = 0; i < 5; i++) {
             // Get Random Indexes for answer and question
             const answerIdx = Math.floor(Math.random() * subject.answers.length);
             const questionIdx = Math.floor(Math.random() * subject.answers[answerIdx].questions.length);
-            // console.log("*** AnswerIdx:", answerIdx, " | questionIx:", questionIdx);
-            // console.log("*** Answer:", subject.answers[answerIdx].info, " | question:", subject.answers[answerIdx].questions[questionIdx]);
-
             // push the indexes to the stack
             copyStack.push([answerIdx, questionIdx]);
         }
         // console.log("stack:", copyStack);
         setStack(copyStack);
+    }
+
+    // **** Handle Submit of Form ********
+    const handleSubmit = (e) => {
+        e.preventDefault();
     }
 
     // **** Initialize the Stack *******************
@@ -38,20 +43,31 @@ const StackMemoryToolView = (props) => {
         <div className='container mt-4'>
             <div className='row p-3 round'>
                 <div className="col bg-white round">
-                    <h2 className='text-primary text-center' >Scrutinize the Stack</h2>
+                    <h2 className='text-center' >Scrutinize the Stack</h2>
+                    <p className='text-center text-info' >Directions: Answer the top question on the stack.  <br/ >Correct answer, reduces the stack, Wrong answer grows the stack.  <br />Reduce the stack to nothing to win the challenge</p>
                     <hr />
                     {/* **** Determine if Tool has started or not ******** */}
                     {
                         toolStarted
-                        // **** Show the Stack ************
-                        ?   <div>
-                                <ShowStackComp answers={subject.answers} stack={stack}  />
+                            // **** Show the Stack ************
+                            ? <div>
+                                <ShowStackComp answers={subject.answers} stack={stack} />
+                                {/* **** Input Form ******* */}
+                                <form onSubmit={e => handleSubmit(e)} >
+                                    <div className='row m-3'>
+                                        <button type="submit" className="btn btn-success round col">
+                                            <strong>Submit Answer</strong>
+                                        </button>
+
+                                    </div>
+                                </form>
                             </div>
-                        :   <div className='stackImg'>
+                            // **** If Tool has not Started, show Start button ********
+                            : <div className='stackImg'>
                                 <div className='row'>
                                     <img className='col round'
-                                        src="https://i.ibb.co/MPPxPYk/Srutineer-start-memory-stack-header.png" 
-                                        alt="Scrut Detective"/>
+                                        src="https://i.ibb.co/MPPxPYk/Srutineer-start-memory-stack-header.png"
+                                        alt="Scrut Detective" />
                                 </div>
                                 <div className='row m-3'>
                                     <button className='col btn btn-warning round'
