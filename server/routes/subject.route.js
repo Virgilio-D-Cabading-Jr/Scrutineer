849111d600f861f4d262338a8335bc99d4057376
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-02-03 06:27:29 -0800

    add create and findAll subject routes in server

--- server/routes/subject.route.js
+++ server/routes/subject.route.js
@@ -3,3 +3,19 @@
 /////////////////////////////////////////////////
 
 const SubjectController = require("../controllers/subject.controller");
+
+module.exports = function(app) {
+    // **** Create ******************************
+    app.post("/api/subjects", SubjectController.create );
+
+    // **** Retrieve ****************************
+    app.get("/api/subjects", SubjectController.findAll );
+    
+    app.get("/api/authors/:id", AuthorController.fineOneAuthor);
+
+    // **** Update ******************************
+    app.put("/api/authors/update/:id", AuthorController.updateAuthor);
+
+    // **** Delete ******************************
+    app.delete("/api/authors/delete/:id", AuthorController.deleteAuthor);
+}
