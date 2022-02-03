2529687e42e1cc464ba879af7a602b581441399a
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-02-03 05:56:25 -0800

    add create to subject controller in server

--- server/controllers/subject.controller.js
+++ server/controllers/subject.controller.js
@@ -7,6 +7,16 @@ const Subject = require("../models/subject.model");
 
 // //// CREATE //////////////////////////////////
 
+module.exports.create = (req, res) => {
+    Subject.create(req.body)
+        .then(
+            newlyCreatedSubject => res.json({ 
+                subject: newlyCreatedSubject,
+                message: "🍻🍻🍻 Create was successful 🍻🍻🍻"
+            })
+        )
+        .catch(err => res.status(400).json(err ));
+    };
 
 // //// RETRIEVE ////////////////////////////////
 
