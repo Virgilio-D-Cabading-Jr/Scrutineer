81b8def00fa166c4072b4e6bfd795480184dabd5
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-02-03 17:44:23 -0800

    add subject name and category to showAllSubjects in client

--- client/src/components/ShowAllSubjectsComp.jsx
+++ client/src/components/ShowAllSubjectsComp.jsx
@@ -10,12 +10,24 @@ const ShowAllSubjectsComp = (props) => {
 
     // //// OUTPUT /////////////////////////////////
     return (<div>
+        {/* <h1>In Show All Subjects</h1> */}
+        {/* <p>subjects: {JSON.stringify(subjects)}</p> */}
         {
-            subjects.map((subject, idx) => {
-                return <div className='row'>
-                    <button className='col btn btn-primary'>{subject.name}</button>
-                </div>
-            })
+            subjects
+            ?   subjects.map((subject, idx) => <div className='row'>
+                    <button className='col btn btn-primary m-2 p-2 round'>
+                        <div className='row'>
+                            <div className='col-8'>
+                                <h3>Subject: {subject.name}</h3>
+                                <p>Category: {subject.category}</p>
+                            </div>
+                            <div className='col-3'>
+                                <img className='img-fluid' src=""
+                            </div>
+                        </div>
+                    </button>
+                </div>)
+            :   <div>Loading . . .</div>
         }
     </div>);
 };
--- client/src/views/HomeView.jsx
+++ client/src/views/HomeView.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import HomeNavBarComp from '../components/HomeNavBarComp';
+import ShowAllSubjectsComp from '../components/ShowAllSubjectsComp';
 
 ///////////////////////////////////////////////////
 //  HOME VIEW
@@ -31,7 +32,7 @@ return (<div>
             <div className='row p-3 round'>
                 <div className="col-md-9 bg-info round show-welcome">
                     <h2>Welcome, Let's Study</h2>
-                    <p>Subjects:{JSON.stringify(subjects)}</p>
+                    {/* <p>Subjects:{JSON.stringify(subjects)}</p> */}
                     <hr />
                     <h3>Math:</h3>
                     <Link to="/subject/636">
@@ -39,7 +40,7 @@ return (<div>
                             <strong>Addition: 1 to 10</strong>
                         </button>
                     </Link>
-                    
+                    <ShowAllSubjectsComp subjects={subjects} />
                 </div>
                 <div className='col-md-3'>
                     <p>&nbsp;</p>
